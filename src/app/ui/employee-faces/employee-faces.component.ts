f389d130d1d89c38f1f5320a4184f91238217802
Justyna Duranowska <justa8866@gmail.com>
2022-11-21 22:16:35 +0100

    Add HttpClient and connect to data

--- src/app/ui/employee-faces/employee-faces.component.ts
+++ src/app/ui/employee-faces/employee-faces.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {EmployeeModel} from "../../model/employee.model";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-employee-faces',
@@ -7,4 +10,6 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EmployeeFacesComponent {
+  constructor(private _client: HttpClient) {}
+  data$: Observable<EmployeeModel[] | null> = this._client.get<EmployeeModel[]>('assets/data/employees.json');
 }
