92d6b2f135f5e32331e4ce08a9d37f6c4a835a7f
Justyna Duranowska <justa8866@gmail.com>
2022-11-22 00:41:53 +0100

    change Model into PersonModel

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {EmployeeModel} from "../model/employee.model";
 import {Observable} from "rxjs";
+import { PersonModel } from '../model/person.model';
 
 @Injectable()
 export class EmployeeService {
   constructor(private _client: HttpClient) {
   }
 
-  getAll(): Observable<EmployeeModel[]>  {
-    return this._client.get<EmployeeModel[]>('assets/data/people.json');
+  getAll(): Observable<PersonModel[]>  {
+    return this._client.get<PersonModel[]>('assets/data/people.json');
   }
 }
