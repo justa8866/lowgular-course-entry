8298659b7db76436836ed2d2dac16a277fe81b37
Justyna Duranowska <justa8866@gmail.com>
2022-11-21 23:01:52 +0100

    change service into employee service

--- src/app/ui/employee-faces/employee-faces.component.ts
+++ src/app/ui/employee-faces/employee-faces.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import {EmployeeModel} from "../../model/employee.model";
 import {Observable} from "rxjs";
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-employee-faces',
@@ -10,6 +10,5 @@ import {Observable} from "rxjs";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EmployeeFacesComponent {
-  constructor(private _client: HttpClient) {}
-  data$: Observable<EmployeeModel[] | null> = this._client.get<EmployeeModel[]>('assets/data/employees.json');
-}
+  constructor( private _employeeService: EmployeeService) { }
+  data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();}
