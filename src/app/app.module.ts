8c762ea3d7827cb303978394cc8f3c41efa582b3
Justyna Duranowska <justa8866@gmail.com>
2022-11-22 09:26:45 +0100

    Adding ProjectListComponentModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
 import {EmployeeFacesComponentModule} from "./ui/employee-faces/employee-faces.component-module";
 import { EmployeeServiceModule } from './services/employee.service-module';
+import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
 
 
 
@@ -17,7 +18,8 @@ import { EmployeeServiceModule } from './services/employee.service-module';
     HttpClientModule,
     EmployeeListComponentModule,
     EmployeeFacesComponentModule,
-    EmployeeServiceModule
+    EmployeeServiceModule,
+    ProjectListComponentModule
 
   ],
   providers: [],
