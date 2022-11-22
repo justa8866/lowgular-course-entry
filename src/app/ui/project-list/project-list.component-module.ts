2569c80cca43c55d35d731bd74cd9fce1b1c7e43
Justyna Duranowska <justa8866@gmail.com>
2022-11-22 09:11:45 +0100

    Adding ProjectListComponent

---
+++ src/app/ui/project-list/project-list.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ProjectListComponent } from './project-list.component';
+
+@NgModule({
+  imports: [],
+  declarations: [ProjectListComponent],
+  providers: [],
+  exports: [ProjectListComponent]
+})
+export class ProjectListComponentModule {
+}
---
+++ src/app/ui/project-list/project-list.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProjectListComponent {
+}
