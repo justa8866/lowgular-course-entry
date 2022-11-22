d8cb5c251223e8da0d5b6689311317503196b005
Justyna Duranowska <justa8866@gmail.com>
2022-11-22 22:35:37 +0100

    adding ReactiveFormsModule

--- src/app/ui/user-form/user-form.component-module.ts
+++ src/app/ui/user-form/user-form.component-module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { UserFormComponent } from './user-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  imports: [],
+    imports: [
+        ReactiveFormsModule
+    ],
   declarations: [UserFormComponent],
   providers: [],
   exports: [UserFormComponent]
