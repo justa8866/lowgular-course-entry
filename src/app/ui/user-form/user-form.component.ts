5edb2f5d008b88d7a0a8d078577551f8366f8a88
Justyna Duranowska <justa8866@gmail.com>
2022-11-22 22:36:38 +0100

    Adding userForm and method onButtonClicked

--- src/app/ui/user-form/user-form.component.ts
+++ src/app/ui/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -7,4 +8,13 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserFormComponent {
+  readonly userForm: FormGroup = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl(),
+    name: new FormControl()
+  })
+
+  onButtonClicked(text: string) {
+    alert('it works! ' + text)
+  }
 }
