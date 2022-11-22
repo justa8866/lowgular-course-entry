46587df6c61b64ddcc7823eebb12b270c8eda5a4
Justyna Duranowska <justa8866@gmail.com>
2022-11-22 15:52:15 +0100

    Adding EmployeeFormComponent

---
+++ src/app/ui/employee-form/employee-form.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { EmployeeFormComponent } from './employee-form.component';
+
+@NgModule({
+  imports: [],
+  declarations: [EmployeeFormComponent],
+  providers: [],
+  exports: [EmployeeFormComponent]
+})
+export class EmployeeFormComponentModule {
+}
