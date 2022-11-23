f7d50ee13143c8f8cc784cc9771c458884e6d675
Justyna Duranowska <justa8866@gmail.com>
2022-11-23 17:56:15 +0100

    task 5 CRUD

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import {map, Observable} from "rxjs";
 import { PersonModel } from '../model/person.model';
 import {CreateEmployeeModel} from "../model/create-employee.model";
+import {ApiResponse} from "./api.response";
+import {EmployeeResponse} from "./employee.response"
 
 @Injectable()
 export class EmployeeService {
@@ -10,7 +12,20 @@ export class EmployeeService {
   }
 
   getAll(): Observable<PersonModel[]>  {
-    return this._client.get<PersonModel[]>('assets/data/people.json');
+    return this._client.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/api/v1/employees')
+      .pipe(map((response: ApiResponse<EmployeeResponse[]>) => {
+        return response.data.map((employeeResponse: EmployeeResponse) => {
+          return {
+            name: employeeResponse.employee_name,
+            personalNumber: employeeResponse.id,
+            img: employeeResponse.profile_image,
+            surname: '',
+            mail: ''
+          };
+        })
+
+      })
+  )
   }
 
   // create(employee: CreateEmployeeModel): Observable<any> {
