5b6bf4df10b559397f3498a4a247971416494966
Justyna Duranowska <justa8866@gmail.com>
2022-11-23 09:00:09 +0100

    event binding onFormSubmitted method use method from service create but earlier check data model

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
+import { CreateEmployeeModel } from '../../model/create-employee.model';
 
 @Component({
   selector: 'app-employee-form',
@@ -8,15 +10,23 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EmployeeFormComponent {
-    readonly employeeForm: FormGroup = new FormGroup({
-      name: new FormControl(null, [Validators.required]),
-      age: new FormControl(null, [Validators.min(0), Validators.required]),
-      salary: new FormControl(null, [Validators.min(0), Validators.required]),
+  readonly employeeForm: FormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    age: new FormControl(null, [Validators.min(0), Validators.required]),
+    salary: new FormControl(null, [Validators.min(0), Validators.required]),
   })
 
-  onButtonClicked(form: {name: string, age: string, salary: string}) {
+  constructor(private _employeeService: EmployeeService) {
+  }
+
+  onButtonClicked(form: { name: string, age: string, salary: string }) {
     alert('User was successfully added to the database. ' + 'Name: ' + form.name +
-    ', Age: ' + form.age+ ', Salary: '+ form.salary)
+      ', Age: ' + form.age + ', Salary: ' + form.salary)
+  }
+
+  onFormSubmitted(form: CreateEmployeeModel) {
+    this._employeeService.create(form)
+
   }
 
 }
